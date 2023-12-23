70efdd13a8e8e35a90f765c56c0521b9e482853f
Khai Nguyen <khai.nguyen@devsamurai.com>
2023-12-23 17:35:40 +0700

    config url

--- admin/src/config/mainConfig.js
+++ admin/src/config/mainConfig.js
@@ -4,6 +4,5 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  BACKEND_URL:
-    'https://class-management-service-vf8y.onrender.com://localhost:8000',
+  BACKEND_URL: 'https://class-management-service-vf8y.onrender.com',
 };
